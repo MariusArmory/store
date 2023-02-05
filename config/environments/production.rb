de2c42159fc3f9cb91a35199f8892fd9db26ad3c
Timothy Marias <timothyjamesmarias@gmail.com>
2023-02-05 11:41:25 -0800

    fixed email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'mariusarmory.com',
+    domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
